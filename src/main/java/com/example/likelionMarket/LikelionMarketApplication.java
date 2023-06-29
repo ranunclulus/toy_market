723fd8a622d68c660719f934b58110d42e4557a0
ranunclulus <morion002@gmail.com>
2023-06-29 11:48:58 +0900

    feat: initialize project

---
+++ src/main/java/com/example/likelionMarket/LikelionMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelionMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikelionMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/likelionMarket/LikelionMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelionMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

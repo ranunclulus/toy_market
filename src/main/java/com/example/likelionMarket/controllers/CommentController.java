48865158246a99ff0d291026a1b19e9577735db9
ranunclulus <morion002@gmail.com>
2023-06-30 00:46:55 +0900

    feat: 정리용 커밋

--- src/main/java/com/example/likelionMarket/controllers/CommentController.java
+++ src/main/java/com/example/likelionMarket/controllers/CommentController.java
@@ -1,13 +1,26 @@
 package com.example.likelionMarket.controllers;
 
+import com.example.likelionMarket.dtos.CommentDto;
+import com.example.likelionMarket.dtos.ResponseDto;
 import com.example.likelionMarket.servicies.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/items/{itemId}/comments")
 public class CommentController {
     private final CommentService commentService;
+
+    // POST /items/{itemId}/comments
+    @PostMapping()
+    public ResponseDto createComment(
+            @RequestParam("itemId") Long itemId,
+            @RequestBody CommentDto commentDto
+            ) {
+        commentService.createComment(itemId, commentDto);
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.setMessage("댓글이 등록되었습니다");
+        return responseDto;
+    }
 }
--- src/main/java/com/example/likelionMarket/servicies/CommentService.java
+++ src/main/java/com/example/likelionMarket/servicies/CommentService.java
@@ -1,11 +1,21 @@
 package com.example.likelionMarket.servicies;
 
+import com.example.likelionMarket.dtos.CommentDto;
 import com.example.likelionMarket.repositories.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.imageio.spi.ServiceRegistry;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final ServiceRegistry serviceRegistry;
+
+    // 새로운 댓글 생성
+    public void createComment(Long itemId, CommentDto commentDto) {
+
+    }
 }

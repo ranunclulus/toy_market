96ba27338775920b1e89c4072b031f7ee75c7816
ranunclulus <morion002@gmail.com>
2023-07-27 11:24:45 +0900

    update: jpa에 user 저장하는 UserDetailsManager 완료

---
+++ src/main/java/com/example/likelionMarket/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.likelionMarket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/register")
+                        .permitAll()
+                        .requestMatchers("/items")
+                        .permitAll()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/example/likelionMarket/controllers/RootController.java
+++ src/main/java/com/example/likelionMarket/controllers/RootController.java
@@ -3,10 +3,15 @@ package com.example.likelionMarket.controllers;
 import com.example.likelionMarket.dtos.CustomUserDetails;
 import com.example.likelionMarket.dtos.ResponseDto;
 import com.example.likelionMarket.entities.UserEntity;
+import com.example.likelionMarket.exceptions.badRequest.PasswordException;
 import com.example.likelionMarket.servicies.JpaUserDetailsManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -16,16 +21,22 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class RootController {
     private final JpaUserDetailsManager manager;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
-    public ResponseDto resister(CustomUserDetails userDetails) {
-        manager.createUser(CustomUserDetails.builder()
-                .username(userDetails.getUsername())
-                .password(userDetails.getPassword())
-                .createAt(LocalDateTime.now())
-                .build());
+    public ResponseDto resister(@RequestParam("username") String username,
+                                @RequestParam("password") String password,
+                                @RequestParam("password-check") String passwordCheck) {
         ResponseDto responseDto = new ResponseDto();
-        responseDto.setMessage("회원 가입이 완료되었습니다");
+        if (password.equals(passwordCheck)) {
+            manager.createUser(CustomUserDetails.builder()
+                    .username(username)
+                    .password(passwordEncoder.encode(password))
+                    .createAt(LocalDateTime.now())
+                    .build());
+            responseDto.setMessage("회원 가입이 완료되었습니다");
+        }
+        else responseDto.setMessage("비밀번호를 확인해 주세요");
         return responseDto;
     }
 }

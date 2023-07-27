e4ba2ec7a77148c963e21fb17e76963cf0de3099
ranunclulus <morion002@gmail.com>
2023-07-27 11:41:43 +0900

    feat: JwtTokenUtils

---
+++ src/main/java/com/example/likelionMarket/utils/JwtTokenUtils.java
@@ -0,0 +1,21 @@
+package com.example.likelionMarket.utils;
+
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Slf4j
+@Component
+public class JwtTokenUtils {
+    private final Key signingKey;
+    public JwtTokenUtils(
+            @Value("${jwt.secret}")
+            String jwtSecret
+    ) {
+        this.signingKey
+                = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+}

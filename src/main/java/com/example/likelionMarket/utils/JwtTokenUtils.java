d08469a006650cb61b3ae00a07b9d7021dd305bf
ranunclulus <morion002@gmail.com>
2023-07-27 11:43:57 +0900

    update:generateToken

--- src/main/java/com/example/likelionMarket/utils/JwtTokenUtils.java
+++ src/main/java/com/example/likelionMarket/utils/JwtTokenUtils.java
@@ -1,11 +1,16 @@
 package com.example.likelionMarket.utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.time.Instant;
+import java.util.Date;
 
 @Slf4j
 @Component
@@ -17,5 +22,18 @@ public class JwtTokenUtils {
     ) {
         this.signingKey
                 = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        // JWT에 담고싶은 추가 정보를 여기에서 추가한다.
+        Claims jwtClaims = Jwts.claims()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now().plusSeconds(1000)));
+
+        return Jwts.builder()
+                .setClaims(jwtClaims)
+                .signWith(signingKey)
+                .compact();
     }
 }
